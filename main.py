5ee3afa819f33cc975de2d97554faa218792d7ce
Adam Power <adam.power48@mail.dcu.ie>
2018-10-08 20:32:36 +0100

    Split checkpoints into models and weights. Updated environment to python3.6 conda

--- main.py
+++ main.py
@@ -15,18 +15,34 @@ FILENAMES_DATASETS = {
     "trump_tweets": "datasets/trump_tweets.txt",
     "trump_speech": "datasets/trump_transcripts/speech_all.txt",
     "trump_tweets_speech": "datasets/trump_tweets_speech.txt",
+    "folklore": "datasets/folklore/all_folklore.txt",
 
 }
 
 FILENAMES_MODELS = {
-    "words": "checkpoints/model_words.hdf5",
-    "chars": "checkpoints/model_chars.hdf5",
-    "dracula_chars": "checkpoints/model_drac_c.hdf5",
-    "dracula_words": "checkpoints/model_drac_w.hdf5",
-    "trump_tweets_chars": "checkpoints/model_trump_tweets_c.hdf5",
-    "trump_speech_chars_small": "checkpoints/model_trump_speech_c_small.hdf5",
-    "trump_speech_chars_large": "checkpoints/model_trump_speech_c_large.hdf5",
-    "trump_tweets_speech": "checkpoints/model_trump_tweets_speech.hdf5",
+    "words": "models/model_words.json",
+    "chars": "models/model_chars.json",
+    "dracula_chars": "models/model_drac_c.json",
+    "dracula_words": "models/model_drac_w.json",
+    "shakespeare_chars": "models/model_shakespeare_c.json",
+    "trump_tweets_chars": "models/model_trump_tweets_c.json",
+    "trump_speech_chars_small": "models/model_trump_speech_c_small.json",
+    "trump_speech_chars_large": "models/model_trump_speech_c_large.json",
+    "trump_tweets_speech": "models/model_trump_tweets_speech.json",
+    "folklore_chars": "models/model_folklore_c.json",
+}
+
+FILENAMES_WEIGHTS = {
+    "words": "weights/model_words.h5",
+    "chars": "weights/model_chars.h5",
+    "dracula_chars": "weights/model_drac_c.h5",
+    "dracula_words": "weights/model_drac_w.h5",
+    "shakespeare_chars": "weights/model_shakespeare_c.h5",
+    "trump_tweets_chars": "weights/model_trump_tweets_c.h5",
+    "trump_speech_chars_small": "weights/model_trump_speech_c_small.h5",
+    "trump_speech_chars_large": "weights/model_trump_speech_c_large.h5",
+    "trump_tweets_speech": "weights/model_trump_tweets_speech.h5",
+    "folklore_chars": "weights/model_folklore_c.h5",
 }
 
 FILENAMES_EMBEDDING = {
@@ -38,12 +54,16 @@ FILENAMES_VOCAB = {
     "trump_tweets_91": "vocabs/trump_91.json",
     "trump_speech_81": "vocabs/trump_81.json",
     "trump_t_s_92": "vocabs/trump_92.json",
+    "dracula": "vocabs/dracula_82.json",
+    "shakespeare": "vocabs/shakespeare_76.json",
+    "folklore": "vocabs/folklore_80.json",
 }
 
 # DATASET PARAMETERS
-model_filename = FILENAMES_MODELS["trump_tweets_speech"]
-data_filename = FILENAMES_DATASETS["trump_tweets_speech"]
-vocab_filename = FILENAMES_VOCAB["trump_t_s_92"]
+model_filename = FILENAMES_MODELS["folklore_chars"]
+weights_filname = FILENAMES_WEIGHTS["folklore_chars"]
+data_filename = FILENAMES_DATASETS["folklore"]
+vocab_filename = FILENAMES_VOCAB["folklore"]
 load_vocab = True
 use_chars = True
 clean = {
@@ -65,7 +85,7 @@ NUM_EPOCHS = 2000
 
 # GENERATION PARAMETERS
 PROB_CUTOFF = 0.9
-DEMO_STEPS = 512
+DEMO_STEPS = 1000
 
 
 def parse_dataset(filename, chars=False, dataset_length=None, max_indices=1000, clean=None, load_vocab=False):
@@ -149,7 +169,8 @@ def filter_n(percentages, n):
 
 def train(resume=False):
     if resume:
-        model = keras.models.load_model(model_filename)
+        model = keras.models.model_from_json(dl.read_file(model_filename))
+        model.load_weights(weights_filname)
     else:
         model = my_models.large_pt_embedding(VOCAB_SIZE, NUM_STEPS, w_i, embedding_filename, freeze_embedding)
 
@@ -158,9 +179,12 @@ def train(resume=False):
                   metrics=[keras.metrics.categorical_accuracy])
 
     model.summary()
-    model.save(model_filename)
+    dl.write_file(model_filename, model.to_json())
 
-    checkpointer = keras.callbacks.ModelCheckpoint(model_filename, verbose=1, period=1, save_best_only=True)
+    exit()
+
+    checkpointer = keras.callbacks.ModelCheckpoint(weights_filname, verbose=1, period=1, save_best_only=True,
+                                                   save_weights_only=True)
     demo_callback = DemoCallback()
 
     model.fit_generator(train_data_generator.generate(), (len(train_data) - NUM_STEPS) // BATCH_SIZE, NUM_EPOCHS,
@@ -225,7 +249,8 @@ def demo(deterministic=False, words=False, num_predict=280):
     sep_char = " " if words else ""
 
     # Build model & dataset
-    model = keras.models.load_model(model_filename)
+    model = keras.models.model_from_json(dl.read_file(model_filename))
+    model.load_weights(weights_filname)
     example_training_generator = dl.RandomSampleGenerator(train_data, NUM_STEPS, 1, VOCAB_SIZE)
 
     # Generate text
@@ -241,11 +266,10 @@ if __name__ == '__main__':
     print("Embedding:", embedding_filename)
 
     if len(sys.argv) < 2:
-        train(resume=True)
+        # train(resume=True)
         # train(resume=False)
-        # demo(False, False, DEMO_STEPS)
+        demo(False, False, DEMO_STEPS)
         exit()
-
     if sys.argv[1] == "demo":
         demo()
     elif sys.argv[1] == "resume":
